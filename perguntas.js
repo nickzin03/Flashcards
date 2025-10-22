8eec3bce1ec59efb0b9355a7708c05255d92e606
NICOLAS GABRIEL DOS SANTOS <oliveira.nicolas01@escola.pr.gov.br>
2025-10-22 00:59:23 +0000

    finalizado

---
+++ perguntas.js
@@ -0,0 +1,73 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Ingles',
+    'como se diz oi em ingles?',
+    'hi(rai)'
+)
+criaCartao(
+    'historia',
+    'quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+criaCartao(
+    'português',
+    'O que e um conto?',
+    'E um texto narrativo curto geralmente com poucos personagens'
+)
+criaCartao(
+    'Matemática',
+    'Oque e uma equação de 1° grau ?',
+    'É uma pergunta que possui uma incógnita'
+)
+criaCartao(
+    'Biologia',
+    'Oque é DNA?',
+    'É uma molécula que contém as informações genéticas de todos os seres vivios.'
+)
+criaCartao(
+    'Química',
+    'Oque é uma reação Química?',
+    'uma reação química é um processo em que uma ou mais substâncias se transformam em outras diferentes'
+)
+criaCartao(
+    'Sociologia',
+    'Oque e uma equação de 1° grau ?',
+    'É uma pergunta que possui uma incógnita'
+)
+criaCartao(
+    'Biologia I',
+    'Oque são lipídios?',
+    'São um grupo de compostos orgânicos que têm como característica principal a sua solubilidade em solventes orgânicos.'
+)
+criaCartao(
+    'Fisica I',
+    'Quais são as principais características da robótica na industria moderna ?',
+    'A robótica tem varias áreas de aplicação na industria moderna.'
+)
+criaCartao(
+    'Matematica II',
+    'Oque é uma variavel?',
+    'E uma conceito fundamental em matemática,programação e outras áreas da ciências .'
+)
+criaCartao(
+    'Projeto de Vida ',
+    'O que é um projeto de vida?',
+    '.'
+)
